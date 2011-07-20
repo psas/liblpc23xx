c37d8e97a3176b2253606f949e7048461e77bfa7
kwilson <kwilsonpdx@gmail.com>
2011-07-19 17:31:47 -0700

    Update to adc code, still developing

--- lpc23xx-adc/include/lpc23xx-adc.h
+++ lpc23xx-adc/include/lpc23xx-adc.h
@@ -8,10 +8,33 @@
 #ifndef _LPC23XX_ADC_H
 #define _LPC23XX_ADC_H
 
+// PCONP
+#define 	PCAD			12
 
-typedef enum { ADC0=0, ADC1, ADC2, ADC3, ADC4, ADC5, ADC6, ADC7 } adc_channel;
+// AD0CR
+#define     PDN             21
+#define     CLKDIV          08
+#define     BURST           16
 
 
+#define     ADC_PWR_ON  	(PCONP = (PCONP | (1<<PCAD)))
+#define     ADC_PWR_OFF  	(PCONP = (PCONP & ~(1<<PCAD)))
+
+#define     ADC_ENABLE      (AD0CR = (AD0CR | (1<<PDN)))
+#define     ADC_DISABLE     (AD0CR = (AD0CR & ~(1<<PDN)))
+
+#define     AD0DR0_DONE     ((AD0DR0 & 0x80000000) >> 31)
+#define     AD0DR1_DONE     ((AD0DR1 & 0x80000000) >> 31)
+#define     AD0DR2_DONE     ((AD0DR1 & 0x80000000) >> 31)
+#define     AD0DR3_DONE     ((AD0DR3 & 0x80000000) >> 31)
+#define     AD0DR4_DONE     ((AD0DR4 & 0x80000000) >> 31)
+#define     AD0DR5_DONE     ((AD0DR5 & 0x80000000) >> 31)
+#define     AD0DR6_DONE     ((AD0DR6 & 0x80000000) >> 31)
+#define     AD0DR7_DONE     ((AD0DR7 & 0x80000000) >> 31)
+
+typedef     uint8_t 		adc_channelmask;
+
+typedef enum {ADC0=0, ADC1, ADC2, ADC3, ADC4, ADC5, ADC6, ADC7} adc_channel;
 
 
 
--- lpc23xx-adc/lpc23xx-adc.c
+++ lpc23xx-adc/lpc23xx-adc.c
@@ -4,51 +4,111 @@
  */
 
 #include <stdint.h>
+#include "lpc23xx.h"
 
+#include "lpc23xx-pll.h"
+#include "lpc23xx-uart.h"
 
 #include "lpc23xx-adc.h"
 
-void adc_init(adc_channel c) {
-    switch(c) {
-        case ADC0:
+/*
+ * adc_init_sw
+ * input: 8 bit channel. each bit set enables
+ *   respective channel
+ * this initializes continuous sampling at
+ * 11 clocks/sample.
+ */
+void adc_init_cont(adc_channelmask channels) {
+	uint8_t cdiv=0;
 
+	// turn on power
+   ADC_PWR_ON;
+   // then enable ADC
+   ADC_ENABLE;
 
-            break;
-        default:
-            break;
-    }
+   // cclk value
+   cclk = pllquery_cclk_mhz();
 
-    
-    
-    
+   switch(cclk) {
+   case FOURTY_EIGHT_MHZ:
+	   cdiv = 11; // ~4.36Mhz
+	   break;
+   case SIXTY_MHZ:
+	   cdiv = 14; // ~4.28Mhz
+	   break;
+   case SEVENTY_TWO_MHZ:
+	   cdiv = 17; // ~4.23Mhz
+	   break;
+   default:
+	   while(1);
+	   break;
+   }
+
+   AD0CR = (AD0CR | (cdiv << CLKDIV) | (1<<BURST) | channels ); // 11 clocks/channel
 }
 
-void adc_disable(ADC_CHANNEL);
-
-void adc0_isr(void){
-
+/*
+ * adc_disable
+ */
+void adc_disable(ADC_CHANNEL) {
+	// order matters see page 596 of user manual v3.
+	ADC_DISABLE;
+	ADC_PWR_OFF;
 }
-//void adc1_isr(void){
-//
-//}
-//void adc2_isr(void){
-//
-//}
-//void adc3_isr(void){
-//
-//}
-//void adc4_isr(void){
-//
-//}
-//void adc5_isr(void){
-//
-//}
 
-//#if (defined LPC2377 || defined LPC2378 || defined LPC2388)
-//    // lpc2377/78 lpc2388 only.
-//    void adc6_isr
-//    void adc7_isr
-//#endif
+/*
+ * adc_read
+ * read value from AD0DRx
+ */
+
+uint16_t adc_read(adc_channel ch) {
+	uint16_t value= 0;
+
+	switch(ch) {
+	case ADC0:
+		do {
+			value = AD0DR0;
+		} while (AD0DR0_DONE == 0);
+		break;
+	case ADC1:
+		do {
+			value = AD0DR1;
+		} while (AD0DR1_DONE == 0);
+		break;
+	case ADC2:
+		do {
+			value = AD0DR2;
+		} while (AD0DR2_DONE == 0);
+		break;
+	case ADC3:
+		do {
+			value = AD0DR3;
+		} while (AD0DR3_DONE == 0);
+		break;
+	case ADC4:
+		do {
+			value = AD0DR4;
+		} while (AD0DR4_DONE == 0);
+		break;
+	case ADC6:
+		do {
+			value = AD0DR6;
+		} while (AD0DR6_DONE == 0);
+		break;
+	case ADC7:
+		do {
+			value = AD0DR7;
+		} while (AD0DR7_DONE == 0);
+		break;
+	default:
+		break;
+	}
+
+	value=(value>>6) & 0x03FF;
+
+	return(value);
+}
+
 
 
 
