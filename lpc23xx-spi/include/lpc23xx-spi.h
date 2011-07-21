c8e9adbf00ff8594a42c54971ae4b12a2c4398e0
kwilson <kwilsonpdx@gmail.com>
2011-07-20 21:55:04 -0700

    Started SPI

---
+++ lpc23xx-spi/include/lpc23xx-spi.h
@@ -0,0 +1,11 @@
+
+/*
+ * lpc23xx-spi.h
+ */
+
+#ifndef _LPC23XX_SPI_H
+#define _LPC23XX_SPI_H
+
+
+
+#endif
---
+++ lpc23xx-spi/lpc23xx-spi.c
@@ -0,0 +1,18 @@
+
+/*
+ * lpc23xx-spi.c
+ */
+
+#include <stdint.h>
+#include <stdlib.h>
+
+#include "lpc23xx.h"
+
+#include "lpc23xx-binsem.h"
+#include "lpc23xx-uart.h"
+#include "lpc23xx-util.h"
+#include "lpc23xx-vic.h"
+
+#include "lpc23xx-spi.h"
+
+
